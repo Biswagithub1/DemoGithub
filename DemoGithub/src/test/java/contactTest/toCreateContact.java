b1e0966feada56785df79614c32720956699747c
Biswa <Biswa@Biswa-HP>
2024-08-12 11:01:31 +0530

    1st push.

---
+++ DemoGithub/src/test/java/contactTest/toCreateContact.java
@@ -0,0 +1,9 @@
+package contactTest;
+
+public class toCreateContact {
+
+	public static void main(String[] args) {
+		System.out.println("contact created");
+	}
+
+}
---
+++ DemoGithub/src/test/java/orgTest/toCreateOrg.java
@@ -0,0 +1,10 @@
+package orgTest;
+
+public class toCreateOrg {
+
+	public static void main(String[] args) {
+		System.out.println("org created");
+
+	}
+
+}
